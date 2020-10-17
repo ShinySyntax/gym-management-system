a2eb22b8f5f28fbf93c7ec1b5b891900afdf28f1
kindlyman343423 <kindlyman343423@gmail.com>
2020-10-17 20:06:09 +0545

    add dummy dashboard cards

--- app/pages/Dashboard/index.tsx
+++ app/pages/Dashboard/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Card, Col, Row } from 'antd';
 import LayoutSidebar from '../../components/Layout/Layout';
 
 const Dashboard = (): JSX.Element => {
   return (
     <LayoutSidebar>
-      <h1>This is Dashboard</h1>
+      <div className="site-card-wrapper">
+        <Row gutter={16} style={{ marginBottom: '2rem' }}>
+          <Col span={8}>
+            <Card title="Total Earning" bordered={false}>
+              <div style={{ backgroundColor: '#ded677' }}>Rs. 80,000</div>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Monthly Earning" bordered={false}>
+              <div style={{ backgroundColor: '#72bfdc' }}>Rs. 15,000</div>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Weekly Earning" bordered={false}>
+              <div style={{ backgroundColor: '#9797e2' }}>Rs. 5,000</div>
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={16} style={{ marginBottom: '2rem' }}>
+          <Col span={8}>
+            <Card title="Total Customers" bordered={false}>
+              <div style={{ backgroundColor: '#ded677' }}>48</div>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Monthly Customers" bordered={false}>
+              <div style={{ backgroundColor: '#72bfdc' }}>8</div>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Weekly Customers" bordered={false}>
+              <div style={{ backgroundColor: '#9797e2' }}>2</div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </LayoutSidebar>
   );
 };
