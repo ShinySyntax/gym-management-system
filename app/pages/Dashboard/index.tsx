866bea39fb3fec3d6e9f838cf6cb102fc696ade3
kindlyman343423 <kindlyman343423@gmail.com>
2020-10-19 09:10:01 +0545

    add dummy notification bar

--- app/pages/Dashboard/index.tsx
+++ app/pages/Dashboard/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Card, Col, Row } from 'antd';
+import path from 'path';
 import LayoutSidebar from '../../components/Layout/Layout';
 
+const notification = {
+  title: 'Notification with image',
+  body: 'Short message plus a custom image',
+  icon: path.join(__dirname, '../../../assets/img/programming.png'),
+};
+
 const Dashboard = (): JSX.Element => {
+  const handleSystemNotification = (): void => {
+    const myNotification = new window.Notification(
+      notification.title,
+      notification
+    );
+
+    myNotification.onclick = () => {
+      console.log('Notification clicked');
+    };
+  };
+
   return (
     <LayoutSidebar>
       <div className="site-card-wrapper">
         <Row gutter={16} style={{ marginBottom: '2rem' }}>
           <Col span={8}>
-            <Card title="Total Earning" bordered={false}>
+            <Card
+              title="Total Earning"
+              bordered={false}
+              onClick={handleSystemNotification}
+            >
               <div style={{ backgroundColor: '#ded677' }}>Rs. 80,000</div>
             </Card>
           </Col>
